ffc7a3430fd265bf22b6fb67b5a79a224a220dfd
erakeun <erakeun@users.noreply.github.com>
2026-09-08 13:57:45 +0900

    fix: point Pages to final Apps Script API

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.WORK_CONFIG = {
   // Apps Script 웹 앱 배포 URL을 넣으면 공용 로그인/DB 모드가 활성화됩니다.
-  API_URL: 'https://script.google.com/macros/s/AKfycby-xhA4VsI0c9zihgrf0aLVxnpTpCBZmTnTAAguuX76Ijj52Kfd_ILdOBFyIv5VPzp6/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw97dr2X_o14hiZKTISTULLibZywd_Xxehcao8A9xMQNf70806BEW4NbipwGC0jte4v/exec',
   SYSTEM_NAME: '서울 근로장학생 관리',
   TERM_NAME: '2026년 2학기'
 };
