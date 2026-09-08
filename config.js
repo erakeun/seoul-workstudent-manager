cb599083b6a63c78b3b0d2e2242ae07526867c2d
erakeun <erakeun@users.noreply.github.com>
2026-09-08 10:13:31 +0900

    chore: update Apps Script API endpoint

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.WORK_CONFIG = {
   // Apps Script 웹 앱 배포 URL을 넣으면 공용 로그인/DB 모드가 활성화됩니다.
-  API_URL: 'https://script.google.com/macros/s/AKfycbwUUlwZrsJG6ByPatE3cvF7UI8y4nVlYQlXbsiynPOAzdimpkRpfWJAWwKi6FX8NhM_/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycby-xhA4VsI0c9zihgrf0aLVxnpTpCBZmTnTAAguuX76Ijj52Kfd_ILdOBFyIv5VPzp6/exec',
   SYSTEM_NAME: '서울 근로장학생 관리',
   TERM_NAME: '2026년 2학기'
 };
